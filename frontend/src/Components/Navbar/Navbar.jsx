2a1121ab486227a2c43f514ee606ae04aa349482
Kerciu <kerciuuu@gmail.com>
2024-11-24 18:39:52 +0100

    initially filled the navbar component according to the ui sketch, created PlusButton component

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
+import PlusButton from "./PlusButton/PlusButton";
+
 const Navbar = () => {
-    return <div></div>
+    return <div className="navbar">
+
+        <div className="navbar-logo">Logo</div>
+
+        <div className="navbar-item">Browse</div>
+
+        <div className="navbar-item">Decks</div>
+
+        <div className="navbar-item">Import</div>
+
+        <div className="navbar-item">Statistics</div>
+
+        <PlusButton/>
+
+    </div>
 }
 
 export default Navbar;
---
+++ frontend/src/Components/Navbar/PlusButton/PlusButton.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const PlusButton = () => {
+
+    const addButtonOpen = () => {
+
+    }
+
+    return <div className="plus-button" onClick={addButtonOpen}>
+        +
+    </div>
+}
+
+export default PlusButton;
