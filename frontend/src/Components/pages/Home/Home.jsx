ae5beb2eb81cc03d4a320fd97a7f1aaf261006e4
Kerciu <kerciuuu@gmail.com>
2024-11-27 21:11:23 +0100

    rendering decks based on whether they are imported or personal on the home page

--- frontend/src/Components/pages/Home/Home.jsx
+++ frontend/src/Components/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Navbar from "../../Navbar/Navbar";
 import Deck from "../../Deck/Deck";
@@ -8,23 +8,41 @@ import './Home.css';
 import testDecks from "../../../assets/test/testDecks";
 
 const Home = (props) => {
+
+    const [myDecksClicked, setMyDecksClicked] = useState(true);
+
     return <div className="home">
+
         <Navbar details={props.details}/>
 
         <div className="home-latest-reviews">
 
             <div className="latest-reviews-title">My Latest Reviews</div>
-
-            {/* when data is fetched, it will be needed to map those using map() */}
-
-            {testDecks.map((deck) => {
-                return <Deck deckState={deck}/>
-            })}
-
+            <div className="latest-reviews-decks">
+                {testDecks.map((deck) => {
+                    return <Deck deckState={deck}/>
+                })}
+            </div>
 
         </div>
 
         <div className="home-my-decks">
+
+            <div className="my-decks-buttons">
+                <div className="my-decks-button">My Decks</div>
+                <div className="my-decks-button">Imported Decks</div>
+            </div>
+
+            {/* będzie w bazie danych flaga od decków personalnych i importowanych */}
+
+            <div className="my-decks-container">
+            {myDecksClicked &&
+                testDecks
+                    .filter(deck => myDecksClicked ? deck.imported === false : deck.imported === true)
+                    .map(deck => (
+                        <Deck deckState={deck}/>
+            ))}
+            </div>
 
         </div>
     </div>
--- frontend/src/assets/test/testDecks.js
+++ frontend/src/assets/test/testDecks.js
@@ -3,21 +3,30 @@ const testDecks = [
     title: "Japanese N5",
     newCards: 50,
     learningCards: 25,
-    reviewingCards: 200
+    reviewingCards: 200,
+    lastReview: "10 days ago",
+    image: null,
+    imported: false
 },
 
 {
     title: "Owoce",
     newCards: 50,
     learningCards: 25,
-    reviewingCards: 200
+    reviewingCards: 200,
+    lastReview: "15 days ago",
+    image: null,
+    imported: false
 },
 
 {
     title: "Warzywa",
     newCards: 50,
     learningCards: 25,
-    reviewingCards: 200
+    reviewingCards: 200,
+    lastReview: "20 days ago",
+    image: null,
+    imported: false
 }
 ]
 
