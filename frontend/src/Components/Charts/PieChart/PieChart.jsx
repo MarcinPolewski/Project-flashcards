4366ded54e51e1e2ae5a5965418a7061b4ba18cb
Kerciu <Kerciuuu@gmail.com>
2024-11-29 18:12:11 +0100

    created pie chart for the new, learning and remembered cards

--- frontend/src/Components/Charts/PieChart/PieChart.jsx
+++ frontend/src/Components/Charts/PieChart/PieChart.jsx
@@ -1,8 +1,34 @@
 import React from "react";
+import { Pie } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import './PieChart.css';
 
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+
 const PieChart = () => {
-    return <div className="pie-chart"></div>
+    const pieData = {
+        labels: ["New", "Learning", "Remember"],
+        datasets: [
+            {
+                data: [149, 43, 2137], /* this will be fetched from api for each deck*/
+                backgroundColor: ["#ff6384", "#36a2eb", "#4bc0c0"],
+                hoverBackgroundColor: ["#ff4d6d", "#3498db", "#26a69a"]
+            }
+        ]
+    };
+
+    const pieOptions = {
+        plugins: {
+            legend: {
+                position: "bottom"
+            }
+        }
+    };
+
+    return <div className="pie-chart-container">
+        <Pie data={pieData} options={pieOptions} />
+    </div>
 }
 
 export default PieChart;
