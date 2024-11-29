0254bb31e88e196af76ba86fb58a171b118e1163
Kerciu <Kerciuuu@gmail.com>
2024-11-29 17:27:56 +0100

    generating random testing data for the github-like mosaic streak chart

--- frontend/src/Components/Charts/StreakChart/StreakChart.jsx
+++ frontend/src/Components/Charts/StreakChart/StreakChart.jsx
@@ -1,6 +1,17 @@
 import React from "react";
 import './StreakChart.css';
 
+const generateSampleData = () => {
+    const MONTHS = 12;
+    const DAYS = 7;
+
+    const data = Array(MONTHS * DAYS).fill(false).map(() => {
+        Math.random() > 0.7
+    });
+
+    return data;
+}
+
 const StreakChart = () => {
     return <div className="streak-chart"></div>
 }
