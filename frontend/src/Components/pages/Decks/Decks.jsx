06b9f52533e9d90f6e23ebd58f88b710d03dfb02
Kerciu <kerciuuu@gmail.com>
2024-11-30 13:15:27 +0100

    created input and filter fields, mapped through all the decks

--- frontend/src/Components/pages/Decks/Decks.jsx
+++ frontend/src/Components/pages/Decks/Decks.jsx
@@ -1,15 +1,33 @@
 import React from "react";
 
 import Navbar from "../../Navbar/Navbar";
+import Deck from "../../Deck/Deck";
 
-import './Decks';
+import testDecks from "../../../assets/test/testDecks";
+
+import './Decks.css';
 
 const Decks = (props) => {
     return <div className="decks">
         <Navbar details={props.details}/>
-        <div>Decks</div>
+        <div className="decks-container">
+
+            <div className="decks-manipulate">
+
+                <input className="decks-input" type="text" />
+                <div className="decks-filter-button">Filter</div>
+
+            </div>
+
+            <div className="decks-list">
+                {testDecks
+                    .map((deck, idx) => (
+                        <Deck key={idx} deckState={deck}/>
+                ))}
+            </div>
+
+        </div>
     </div>
 }
 
 export default Decks;
-
