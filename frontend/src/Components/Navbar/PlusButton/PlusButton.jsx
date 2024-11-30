ea2351529caf642a73e7c51104a3f25e9babda73
Kerciu <kerciuuu@gmail.com>
2024-11-30 17:16:24 +0100

    made use of useOverlay in the PlusButton component in order to create create-deck overlay

--- frontend/src/Components/Navbar/PlusButton/PlusButton.jsx
+++ frontend/src/Components/Navbar/PlusButton/PlusButton.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
+
 import './PlusButton.css';
 
 import { useOverlay } from "../../../contexts/OverlayContext/OverlayContext";
 import { useNavigate } from "react-router-dom";
 
+import Overlay from "../../Overlay/Overlay";
+
 const PlusButton = () => {
 
     const navigate = useNavigate();
     const [isPopupOpen, setPopupOpen] = useState(false);
-    const { toggleOverlay } = useOverlay();
+    const { isOverlayOpen, toggleOverlay, closeOverlay } = useOverlay();
 
     const handleTogglePopup = () => {
         setPopupOpen(!isPopupOpen);
@@ -34,13 +37,24 @@ const PlusButton = () => {
         </div>
 
         {isPopupOpen && (
-                <div className="plus-button-popup popup">
-                    <ul>
-                        <li onClick={() => navigate("/create-flashcard")}>Create Flashcard</li>
-                        <li onClick={toggleOverlay}>Create Deck</li>
-                    </ul>
-                </div>
-            )}
+            <div className="plus-button-popup popup">
+                <ul>
+                    <li onClick={() => navigate("/create-flashcard")}>Create Flashcard</li>
+                    <li onClick={toggleOverlay}>Create Deck</li>
+                </ul>
+            </div>
+        )}
+
+        <Overlay isOpen={isOverlayOpen} closeOverlay={closeOverlay}>
+            <h2>Create a New Deck</h2>
+            <form>
+                <label>
+                    Deck Name:
+                    <input type="text" placeholder="Enter deck name" />
+                </label>
+                <button type="submit">Create</button>
+            </form>
+        </Overlay>
 
     </div>
 }
