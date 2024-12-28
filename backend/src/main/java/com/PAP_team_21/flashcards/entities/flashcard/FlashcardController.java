2414fc0ae5058a3c288ddc9bc9d480c6d23d8287
mcieslik <01187214@pw.edu.pl>
2024-12-28 22:08:55 +0100

    Added FlashcardController class and endpoints service

---
+++ backend/src/main/java/com/PAP_team_21/flashcards/entities/flashcard/FlashcardController.java
@@ -0,0 +1,50 @@
+package com.PAP_team_21.flashcards.entities.flashcard;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/flashcards")
+public class FlashcardController {
+
+    private final FlashcardDao flashcardDao;
+
+    @Autowired
+    public FlashcardController(FlashcardDao flashcardDao) {
+        this.flashcardDao = flashcardDao;
+    }
+
+    @GetMapping("/{id}")
+    public Flashcard getFlashcard(@PathVariable int id) {
+        return flashcardDao.findFlashcardById(id);
+    }
+
+    @GetMapping("/deck_id/{deckId}")
+    public List<Flashcard> getFlashcardsByDeckId(@PathVariable int deckId) {
+        return flashcardDao.findAllInDeck(deckId);
+    }
+
+    @GetMapping
+    public List<Flashcard> getAllFlashcards() {
+        return flashcardDao.findAllFlashcards();
+    }
+
+    @PostMapping
+    public void saveFlashcard(@RequestBody Flashcard flashcard) {
+        flashcardDao.save(flashcard);
+    }
+
+    @PostMapping("/{id}")
+    public void updateFlashcard(@PathVariable int id, @RequestBody Flashcard flashcard) {
+        flashcard.setId(id);
+        flashcardDao.save(flashcard);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFlashcard(@PathVariable int id) {
+        flashcardDao.deleteFlashcardById(id);
+    }
+
+}
