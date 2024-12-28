68f8c01da0771eb6db77fa96ed1a1cbb587903d7
mcieslik <01187214@pw.edu.pl>
2024-12-28 22:19:08 +0100

    Added DeckController class and endpoints service

---
+++ backend/src/main/java/com/PAP_team_21/flashcards/entities/deck/DeckController.java
@@ -0,0 +1,44 @@
+package com.PAP_team_21.flashcards.entities.deck;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/decks")
+public class DeckController {
+
+    private final DeckDao deckDao;
+
+    @Autowired
+    public DeckController(DeckDao deckDao) {
+        this.deckDao = deckDao;
+    }
+
+    @GetMapping("/{id}")
+    public Deck getDeck(@PathVariable int id) {
+        return deckDao.findDeckById(id);
+    }
+
+    @GetMapping
+    public List<Deck> getAllDecks() {
+        return deckDao.findAllDecks();
+    }
+
+    @PostMapping
+    public void saveDeck(@RequestBody Deck deck) {
+        deckDao.save(deck);
+    }
+
+    @PostMapping("/{id}")
+    public void updateDeck(@PathVariable int id, @RequestBody Deck deck) {
+        deck.setId(id);
+        deckDao.save(deck);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDeck(@PathVariable int id) {
+        deckDao.deleteDeckById(id);
+    }
+}
