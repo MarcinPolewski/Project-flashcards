f60e38dcac40dfa1d304898ddeed082632afd6fb
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-12-28 17:00:30 +0100

    created json view config

---
+++ backend/src/main/java/com/PAP_team_21/flashcards/entities/JsonViewConfig.java
@@ -0,0 +1,14 @@
+package com.PAP_team_21.flashcards.entities;
+import com.fasterxml.jackson.annotation.JsonView;
+
+
+
+public class JsonViewConfig {
+    public static class Public {}
+    public static class Internal extends Public {}
+
+    public static class BasicStructures extends Internal {}
+
+    public static class FolderExtended extends Internal {}
+
+}
