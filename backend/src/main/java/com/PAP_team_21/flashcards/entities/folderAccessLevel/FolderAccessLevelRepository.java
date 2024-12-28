c0c5d216df00d12e42ec7d233353485bd2e31152
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-12-28 17:36:05 +0100

    implemented FolderAccessLevelRepository

---
+++ backend/src/main/java/com/PAP_team_21/flashcards/entities/folderAccessLevel/FolderAccessLevelRepository.java
@@ -0,0 +1,6 @@
+package com.PAP_team_21.flashcards.entities.folderAccessLevel;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderAccessLevelRepository extends JpaRepository<FolderAccessLevel, Integer> {
+}
