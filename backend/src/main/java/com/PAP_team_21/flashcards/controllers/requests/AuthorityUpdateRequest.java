e0a360ad99ce6adec617eb0312874a187d2d61e7
mcieslik <01187214@pw.edu.pl>
2024-12-30 17:41:18 +0100

    Added AuthorityCreationRequest and AuthorityUpdateRequest files

---
+++ backend/src/main/java/com/PAP_team_21/flashcards/controllers/requests/AuthorityUpdateRequest.java
@@ -0,0 +1,14 @@
+package com.PAP_team_21.flashcards.controllers.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthorityUpdateRequest {
+    String name;
+}
