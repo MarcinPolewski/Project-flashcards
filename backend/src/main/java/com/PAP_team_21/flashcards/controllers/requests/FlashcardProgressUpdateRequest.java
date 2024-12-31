c350a6d833c2f17bea91c8820d94e01ab8092d38
mcieslik <01187214@pw.edu.pl>
2024-12-31 13:30:50 +0100

    Added FlashcardProgressCreateRequest and FlashcardProgressUpdateRequest files

---
+++ backend/src/main/java/com/PAP_team_21/flashcards/controllers/requests/FlashcardProgressUpdateRequest.java
@@ -0,0 +1,18 @@
+package com.PAP_team_21.flashcards.controllers.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FlashcardProgressUpdateRequest {
+    int flashcardProgressId;
+    LocalDateTime nextReview;
+    boolean valid;
+}
