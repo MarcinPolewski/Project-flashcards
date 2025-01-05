e291f32dff2613e4a9f6c2a71787044e4af32534
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2025-01-05 16:28:32 +0100

    enhanced readability

--- backend/src/main/java/com/PAP_team_21/flashcards/ReviewService/ReviewService.java
+++ backend/src/main/java/com/PAP_team_21/flashcards/ReviewService/ReviewService.java
@@ -4,7 +4,7 @@ import com.PAP_team_21.flashcards.UserAnswer;
 import com.PAP_team_21.flashcards.entities.customer.Customer;
 import com.PAP_team_21.flashcards.entities.deck.Deck;
 import com.PAP_team_21.flashcards.entities.flashcard.Flashcard;
-import com.PAP_team_21.flashcards.entities.flashcard.FlashcardRepository;
+import com.PAP_team_21.flashcards.entities.flashcard.FlashcardService;
 import com.PAP_team_21.flashcards.entities.folder.Folder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -19,27 +19,32 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ReviewService {
 
-    private final FlashcardRepository flashcardRepository;
+    private final FlashcardService flashcardService;
 
 
     public List<Flashcard> reviewBy(Customer customer, Deck deck, int batchSize)
     {
-        // find all currently due flashcards
-
-        // in subset of those find ones that are of category A
-
         // !!! to be defined later !!!
-        int time_constant = 30;
+        int review_gap_constant = 30;
+        int last_review_constant = 30;
         int max_currently_learning = 20; // max number of flashcards that are repeated in learning phase
         float learning_ratio = 0.7f; // ratio of flashcards that are repeated in learning phase
 
         // flashcards in learining, including those that are not due
-        int totalInLearning = flashcardRepository.countCurrentlyLearning(customer.getId(), deck.getId(), time_constant);
-
+        int totalInLearningCnt = flashcardService.countCurrentlyLearning(customer.getId(),
+                deck.getId(),
+                review_gap_constant,
+                last_review_constant);
         // flashcards that are due and in learning phase
-        int dueInLearningCnt = flashcardRepository.countDueInLearning(customer.getId(), deck.getId(), time_constant);
+        int dueInLearningCnt = flashcardService.countDueInLearning(customer.getId(),
+                deck.getId(),
+                review_gap_constant,
+                last_review_constant);
         // flashcards that are due, but are not in review phase - they are well enough known
-        int dueToReviewCnt = flashcardRepository.countDueToReview(customer.getId(), deck.getId(), time_constant);
+        int dueToReviewCnt = flashcardService.countDueToReview(customer.getId(),
+                deck.getId(),
+                review_gap_constant,
+                last_review_constant);
         int totalDue = dueToReviewCnt + dueInLearningCnt;
 
         ArrayList<Flashcard> result = new ArrayList<>();
@@ -48,30 +53,38 @@ public class ReviewService {
         int typeBFlashcardsCnt = batchSize - typeAFlashcardsCnt;
 
 
-        if(totalDue < batchSize)
+        if(totalDue < batchSize) // due cards do not fill the batch
         {
             // general idea:
             // return all due flashcards
             // try to introduce new cards
             // fill the rest with early reviews ?
 
-            List<Flashcard> dueFlashcards = flashcardRepository.getDueFlashcards(customer.getId(), deck.getId(), Pageable.unpaged());
+            List<Flashcard> dueFlashcards = flashcardService.getDueFlashcards(customer.getId(),
+                    deck.getId(),
+                    Pageable.unpaged());
             result.addAll(dueFlashcards);
 
             int howManyLeftToAdd = batchSize - dueFlashcards.size();
-            int howManyNewCardsCanBeIntroduced = max_currently_learning - totalInLearning;
+            int howManyNewCardsCanBeIntroduced = max_currently_learning - totalInLearningCnt;
 
             if(howManyNewCardsCanBeIntroduced > 0)
             {
                 int howManyNewToAdd = Math.min(howManyLeftToAdd, howManyNewCardsCanBeIntroduced);
-                List<Flashcard> newFlashcards = flashcardService.getNewFlashcards(customer, deck, howManyNewToAdd);
+                List<Flashcard> newFlashcards = flashcardService.getNewFlashcards(customer,
+                        deck,
+                        howManyNewToAdd);
                 result.addAll(newFlashcards);
             }
 
             howManyLeftToAdd = batchSize - result.size();
             if(howManyLeftToAdd > 0)
             {
-                List<Flashcard> earlyReviewFlashcards = flashcardRepository.getEarlyReviewFlashcards(customer.getId(), deck.getId(), time_constant,  Pageable.ofSize(howManyLeftToAdd);
+                List<Flashcard> earlyReviewFlashcards = flashcardService.getEarlyReviewFlashcards(customer.getId(),
+                        deck.getId(),
+                        review_gap_constant,
+                        last_review_constant,
+                        Pageable.ofSize(howManyLeftToAdd));
                 result.addAll(earlyReviewFlashcards);
             }
 
@@ -84,32 +97,48 @@ public class ReviewService {
             // try to fill the rest with typeB
             // fill the rest with remaing flashcards of typeA ( theoretically, this is possible or all the list is filled)
 
-            List<Flashcard> dueInLearning = flashcardRepository.getDueInLearning(customer.getId(), deck.getId(), time_constant, Pageable.size(typeAFlashcardsCnt));
+            List<Flashcard> dueInLearning = flashcardService.getDueInLearning(customer.getId(),
+                    deck.getId(),
+                    review_gap_constant,
+                    last_review_constant,
+                    Pageable.ofSize(typeAFlashcardsCnt));
             if(dueInLearning.size() <  typeAFlashcardsCnt)
             {
                 // try to introduce new flashcards
-                int howManyNewCardsCanBeIntroduced =Math.min(max_currently_learning - totalInLearning,
+                int howManyNewCardsCanBeIntroduced =Math.min(max_currently_learning - totalInLearningCnt,
                                                             typeAFlashcardsCnt - dueInLearning.size());
-                List<Flashcard> newFlashcards = flashcardService.getNewFlashcards(customer, deck, howManyNewCardsCanBeIntroduced);
+                List<Flashcard> newFlashcards = flashcardService.getNewFlashcards(customer,
+                        deck,
+                        howManyNewCardsCanBeIntroduced);
                 result.addAll(newFlashcards);
             }
 
             // try to fill the rest of the list with typeB flashcards
             int howManyLeftToAdd = batchSize - result.size();
-            List<Flashcard> dueToReview = flashcardRepository.getDueToReview(customer.getId(), deck.getId(), time_constant, Pageable.size(howManyLeftToAdd));
+            List<Flashcard> dueToReview = flashcardService.getDueToReview(customer.getId(),
+                    deck.getId(),
+                    review_gap_constant,
+                    last_review_constant,
+                    Pageable.ofSize(howManyLeftToAdd));
             result.addAll(dueToReview);
 
             // if there is still space, fill with type A or new flashcards
             howManyLeftToAdd = batchSize - result.size();
             if(howManyLeftToAdd > 0)
             {
-                dueInLearning = flashcardRepository.getDueInLearning(customer.getId(), deck.getId(), time_constant, Pageable.size(howManyLeftToAdd));
+                dueInLearning = flashcardService.getDueInLearning(customer.getId(),
+                        deck.getId(),
+                        review_gap_constant,
+                        last_review_constant,
+                        Pageable.ofSize(howManyLeftToAdd));
                 if(dueInLearning.size() <  howManyLeftToAdd)
                 {
                     // try to introduce new flashcards
-                    int howManyNewCardsCanBeIntroduced =Math.min(max_currently_learning - totalInLearning,
+                    int howManyNewCardsCanBeIntroduced =Math.min(max_currently_learning - totalInLearningCnt,
                             howManyLeftToAdd - dueInLearning.size());
-                    List<Flashcard> newFlashcards = flashcardService.getNewFlashcards(customer, deck, howManyNewCardsCanBeIntroduced);
+                    List<Flashcard> newFlashcards = flashcardService.getNewFlashcards(customer,
+                            deck,
+                            howManyNewCardsCanBeIntroduced);
                     result.addAll(newFlashcards);
                 }
             }
