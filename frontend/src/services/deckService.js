eb12f7396918bb021763eb260ce4331aef416f8f
Kerciu <kerciuuu@gmail.com>
2025-01-05 18:34:39 +0100

    created customer, deck and review services

---
+++ frontend/src/services/deckService.js
@@ -0,0 +1,22 @@
+import api from "../api/api";
+
+export const DeckService = {
+    getFlashcards: async (deckId, page = 0, size = 5, sortBy = 'id', ascending = true) => {
+        const response = await api.get('/deck/flashcards', {
+            params: { deckId, page, size, sortBy, ascending }
+        });
+        return response.data;
+    },
+    createDeck: async (folderId, name) => {
+        const response = await api.post('/deck/create', { folderId, name });
+        return response.data;
+    },
+    updateDeck: async (deckId, name) => {
+        const response = await api.put('/deck/update', { deckId, name });
+        return response.data;
+    },
+    deleteDeck: async (deckId) => {
+        const response = await api.delete('/deck/delete', { params: { deckId } });
+        return response.data;
+    }
+};
---
+++ frontend/src/services/reviewService.js
@@ -0,0 +1,12 @@
+import api from "../api/api";
+
+export const ReviewService = {
+    requestReview: async (deckId, packageSize) => {
+        const response = await api.post('/folder/requestReview', { deckId, packageSize });
+        return response.data;
+    },
+    sendBackResults: async (flashcardId, userAnswer) => {
+        const response = await api.post('/folder/sendBackResults', { flashcardId, userAnswer });
+        return response.data;
+    }
+};
