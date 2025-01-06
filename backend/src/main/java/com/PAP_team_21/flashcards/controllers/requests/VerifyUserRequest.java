77e4cb019638a0b042420c83efac6872056b97bd
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2025-01-06 19:25:01 +0100

    added new requests/DTO and exceptions

---
+++ backend/src/main/java/com/PAP_team_21/flashcards/controllers/requests/VerifyUserRequest.java
@@ -0,0 +1,14 @@
+package com.PAP_team_21.flashcards.controllers.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class VerifyUserRequest {
+    private String code;
+}
