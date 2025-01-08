23ecf7289a0ea287f7427f39d460f1a6efe58da2
Julia Czosnek <julia.czosnek.stud@pw.edu.pl>
2025-01-08 16:58:00 +0100

    created mockDeck

---
+++ frontend/src/assets/mockData/mockDeck.js
@@ -0,0 +1,28 @@
+const spanishN5MockDeck = {
+    id: 5,
+    title: "Spanish N5",
+    newCards: [
+        { id: 1, front: "Hola", back: "Hello", lastReviewed: null },
+        { id: 2, front: "Gracias", back: "Thank you", lastReviewed: null },
+        { id: 3, front: "Adiós", back: "Goodbye", lastReviewed: null },
+        { id: 4, front: "Por favor", back: "Please", lastReviewed: null },
+        { id: 5, front: "Buenos días", back: "Good morning", lastReviewed: null },
+    ],
+    learningCards: [
+        { id: 6, front: "La casa", back: "The house", lastReviewed: "2023-11-15" },
+        { id: 7, front: "El coche", back: "The car", lastReviewed: "2023-11-16" },
+    ],
+    reviewingCards: [
+        { id: 8, front: "El gato", back: "The cat", lastReviewed: "2023-11-14" },
+        { id: 9, front: "El perro", back: "The dog", lastReviewed: "2023-11-13" },
+        { id: 10, front: "El libro", back: "The book", lastReviewed: "2023-11-12" },
+    ],
+    progress: 72,
+    image: null,
+    imported: false,
+    createdAt: "2023-09-25",
+    lastUsed: "2023-11-18",
+    lastModified: "2023-11-07"
+};
+
+export default spanishN5MockDeck;
