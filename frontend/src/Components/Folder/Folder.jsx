cfc675987226d347502e1ea5d0c09c3460d75e22
Julia Czosnek <julia.czosnek.stud@pw.edu.pl>
2025-01-12 21:17:53 +0100

    made folder pretty and responsive

--- frontend/src/Components/Folder/Folder.jsx
+++ frontend/src/Components/Folder/Folder.jsx
@@ -1,24 +1,27 @@
 import React from "react";
-
 import './Folder.css';
 import { useNavigate } from "react-router-dom";
 
-const Folder = ( { id, title } ) => {
-
+const Folder = ({ id, title }) => {
     const navigate = useNavigate();
 
     const handleClick = () => {
-        navigate(`/folder/${id}`)
+        navigate(`/folder/${id}`);
     }
 
-    return <div className="folder">
-        <div className="card-title">{title}</div>
+    return (
+        <div className="folder">
+            <div className="folder-header">
+                <div className="folder-title">{title}</div>
+            </div>
 
-
-        <button className="folder-page-btn folder-page-study-btn" onClick={handleClick}>Open</button>
-        <button className="folder-page-btn folder-page-edit-btn">Edit</button>
-        <button className="folder-page-btn folder-page-delete-btn">Delete</button>
-    </div>
+            <div className="folder-actions">
+                <button className="folder-page-btn folder-page-study-btn" onClick={handleClick}>Open</button>
+                <button className="folder-page-btn folder-page-edit-btn">Edit</button>
+                <button className="folder-page-btn folder-page-delete-btn">Delete</button>
+            </div>
+        </div>
+    );
 }
 
 export default Folder;
