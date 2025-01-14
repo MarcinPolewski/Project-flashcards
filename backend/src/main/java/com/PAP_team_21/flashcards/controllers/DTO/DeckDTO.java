847cd4e1d5fca7c0dfaa23e9ab761fdbf876fb71
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2025-01-14 12:32:50 +0100

    implemented DTO and Mapper classes for Deck

---
+++ backend/src/main/java/com/PAP_team_21/flashcards/controllers/DTO/DeckDTO.java
@@ -0,0 +1,17 @@
+package com.PAP_team_21.flashcards.controllers.DTO;
+
+import com.PAP_team_21.flashcards.entities.JsonViewConfig;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@JsonView(JsonViewConfig.Public.class)
+public class DeckDTO {
+    private int id;
+    private String name;
+    private float progress;
+}
