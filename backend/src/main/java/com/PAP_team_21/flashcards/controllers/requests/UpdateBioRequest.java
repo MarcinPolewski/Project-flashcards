701adc44f4f69b946ce80d3383256f3572872637
mcieslik <01187214@pw.edu.pl>
2025-01-17 20:40:52 +0100

    created UpdateBioRequest

---
+++ backend/src/main/java/com/PAP_team_21/flashcards/controllers/requests/UpdateBioRequest.java
@@ -0,0 +1,14 @@
+package com.PAP_team_21.flashcards.controllers.requests;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UpdateBioRequest {
+    String bio;
+}
