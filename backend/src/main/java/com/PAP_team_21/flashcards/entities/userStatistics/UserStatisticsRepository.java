90e151fbf396aa5042d307b1e2bfea4d778069a5
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2025-01-19 22:44:17 +0100

    fixed repository methyod name

--- backend/src/main/java/com/PAP_team_21/flashcards/entities/userStatistics/UserStatisticsRepository.java
+++ backend/src/main/java/com/PAP_team_21/flashcards/entities/userStatistics/UserStatisticsRepository.java
@@ -1,17 +1,19 @@
 package com.PAP_team_21.flashcards.entities.userStatistics;
 
+import org.springframework.cglib.core.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserStatisticsRepository extends JpaRepository<UserStatistics, Integer> {
 
     @Procedure(procedureName = "get_github_style_chart_data")
-    Object getGithubStyleChartData(@Param("customerId")Integer customerId);
+    List<LocalDateTime> getGithubStyleChartData(@Param("customerId")Integer customerId);
 
 
     @Procedure(procedureName = "find_customers_last_review")
