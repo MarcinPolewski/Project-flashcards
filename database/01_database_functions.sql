4eb93311dc6abfd603664bf967407c16c3f99bfc
Julia Czosnek <julia.czosnek.stud@pw.edu.pl>
2025-01-20 22:06:58 +0100

    added flashcard progress seq

--- database/01_database_functions.sql
+++ database/01_database_functions.sql
@@ -495,5 +495,13 @@ MAXVALUE 999
 CYCLE
 CACHE 10;
 
+CREATE SEQUENCE flashcard_progress_seq
+START WITH 1000
+INCREMENT BY 1
+MINVALUE 1000
+MAXVALUE 9999
+CYCLE
+CACHE 20;
+
 # =============================================================================
 DELIMITER ;
